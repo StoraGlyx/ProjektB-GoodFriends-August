aeb07f133ca9e3c0e8d2dc480bc9f20917ce5c00
August Dahlström Norin <storaglyx@gmail.com>
2026-05-11 19:24:38 +0200

    Add delete quote modal

--- AppRazor/Pages/FriendDetails.cshtml.cs
+++ AppRazor/Pages/FriendDetails.cshtml.cs
@@ -10,14 +10,18 @@ public class FriendDetailsModel : PageModel
     private readonly IFriendsService _friendsService;
     private readonly IPetsService _petsService;
 
+    private readonly IQuotesService _quotesService;
+
     public IFriend? Friend { get; set; }
 
     public FriendDetailsModel(
         IFriendsService friendsService,
-        IPetsService petsService)
+        IPetsService petsService,
+        IQuotesService quotesService)
     {
         _friendsService = friendsService;
         _petsService = petsService;
+        _quotesService = quotesService;
     }
 
     public async Task<IActionResult> OnGet(Guid id)
@@ -37,6 +41,13 @@ public class FriendDetailsModel : PageModel
     public async Task<IActionResult> OnPostDeletePet(Guid friendId, Guid petId)
     {
         await _petsService.DeletePetAsync(petId);
+
+        return RedirectToPage("/FriendDetails", new { id = friendId });
+    }
+
+    public async Task<IActionResult> OnPostDeleteQuote(Guid friendId, Guid quoteId)
+    {
+        await _quotesService.DeleteQuoteAsync(quoteId);
 
         return RedirectToPage("/FriendDetails", new { id = friendId });
     }
